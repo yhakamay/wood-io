0a53ced907937748e0f4683f5c870225865bfc7f
Yusuke <yhakamaya@adobe.com>
2023-04-29 14:41:23 +0900

    add Providers
    https://chakra-ui.com/getting-started/nextjs-guide#app-directory-setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+// https://chakra-ui.com/getting-started/nextjs-guide#app-directory-setup
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
