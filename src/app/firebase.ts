c1fcdd0bfd9351b199c24fd0ba19434e69f7ff5d
Yusuke <yhakamaya@adobe.com>
2023-05-13 21:46:05 +0900

    add firebase.ts

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBSE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBSE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
